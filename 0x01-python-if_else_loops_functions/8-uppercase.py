eb8cc4b04aa25896cd840c0fd38275f6ad868611
Bunmi Akinola <bunconice@gmail.com>
2023-09-15 15:55:56 +0100

    updated

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@ def uppercase(str):
     for i in str:
         if 97 <= ord(i) <= 122:
             i = chr(ord(i) - 32)
-            print("{}".format(i), end='')
         else:
-            print("{}".format(i), end='')
+            i = i
+        print("{}".format(i), end='')
     print()
