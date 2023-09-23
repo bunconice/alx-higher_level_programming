7f92fc2bf7e06a83f4fb7dcfe476d7da1f1d1770
Bunmi Akinola <bunconice@gmail.com>
2023-09-24 00:00:39 +0100

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ =="__main__":
     import hidden_4
-    d = dir
-    for i in range(0, len(d)):
-        if d[i][0:2] != "__":
-            print("{}".format(d[i]))
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
