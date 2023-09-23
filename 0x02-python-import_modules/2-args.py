b0f3f5321befa4356cd80a35920ccf6a33d069c7
Bunmi Akinola <bunconice@gmail.com>
2023-09-23 23:06:17 +0100

    modified

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
 	import sys
-	size = len(sys.argv)
-	if size == 1:
-		print(size - 1, "Arguments.")
-	elif size == 2:
-		print(size - 1, "Argument:")
+	size = len(sys.argv) - 1
+	if size == 0:
+		print(size, "Arguments.")
+	elif size == 1:
+		print(size, "Argument:")
 	else:
-		print(size - 1, "Arguments:")
-	for i in range(1, size):
+		print(size, "Arguments:")
+	for i in range(1, size + 1):
 		print(i, ": ", sys.argv[i], sep="")
