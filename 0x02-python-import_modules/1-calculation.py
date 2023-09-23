abc4ee897557abf0b19fb6aad39236f7314912a5
Bunmi Akinola <bunconice@gmail.com>
2023-09-23 20:47:38 +0100

    task 2

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+a = 10
+b = 5
+
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    c = add(a, b)
+    d = sub(a, b)
+    e = mul(a, b)
+    f = div(a, b)
+    print("{:d} + {:d} = {:d}".format(a, b, c))
+    print("{:d} - {:d} = {:d}".format(a, b, d))
+    print("{:d} * {:d} = {:d}".format(a, b, e))
+    print("{:d} / {:d} = {:d}".format(a, b, f))
