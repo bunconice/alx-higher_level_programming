df7cbcdf7cb37de532357c9bfd49c3b6190bd2f1
Bunmi Akinola <bunconice@gmail.com>
2023-09-23 16:40:56 +0100

    updated

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 a = 1
 b = 2
-
 if __name__ == "__main__":
     from add_0 import add
     c = add(a, b)
-    print("{} + {} = {} ".format(a, b, c))
+    print("{:d} + {:d} = {:d} ".format(a, b, c))
