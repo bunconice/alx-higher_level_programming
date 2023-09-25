cad9908f1cb15a6958a94cc9e54fb488e7e34843
Bunmi Akinola <bunconice@gmail.com>
2023-09-25 21:43:15 +0100

    a function that divides 2 integers and prints the result

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        result = a/b
+    except ZeroDivisionError:
+        result = None
+    else:
+        return result
+    finally:
+        print("inside result: ", result)
+    return result
