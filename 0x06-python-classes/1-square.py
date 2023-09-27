551b1bb385b9729180db58f333cb22389b3afc4a
Bunmi Akinola <bunconice@gmail.com>
2023-09-27 23:41:37 +0100

    Square class with private instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""a class Square that defines a square"""
+
+
+class Square:
+    """Square class with private instance attribute"""
+    def __init__(self, size):
+        self.__size = size
