1e4064adbfad060b4260e1a9174976ad224c0235
Bunmi Akinola <bunconice@gmail.com>
2023-09-28 00:01:34 +0100

    checks size type and if it's greater than zero before assigning it to __size

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""a class Square with private instance attribute: size"""
+
+
+class Square:
+    """private instance attribute: size"""
+    def __init__(self, size=0):
+        """check if size is an integer"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        """check if size is greater than zero"""
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        """Assign size to the private attribute __size"""
+        self.__size = size
