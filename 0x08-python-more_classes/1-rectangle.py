09fc490faa3433a5817a09d7e854982ada46a307
Bunmi Akinola <bunconice@gmail.com>
2023-10-02 13:59:58 +0100

    updated

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,46 +1,46 @@
 #!/usr/bin/python3
-"""a class Rectangle that defines a rectangle by: (based on 0-rectangle.py)"""
+"""A class that defines a rectangle"""
 
 
 class Rectangle:
-    """Rectangle class with private instance attributes"""
+    """this represents a rectangle"""
+
     def __init__(self, width=0, height=0):
-        """initializing the Rectangle class
+        """Initializing this rectangle class
         Args:
-            width: represenst width of the rectangle
+            width: represents the width of the rectangle
             height: represents the height of the rectangle
+        Raises:
+            TypeError: if size is not integer
+            ValueError: if size is less than zero
         """
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """gets the value of width"""
+        """retrieves width attribute"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """checks if value is not an int"""
+        """sets width attribute"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        """checks if value is less than zero"""
         if value < 0:
-            raise ValueError("width must be >= than 0")
-        """assign value to private attribute width"""
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
     def height(self):
-        """gets the value of height"""
+        """retrieves height attribute"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """checks if value is not an int"""
+        """sets height attribute"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        """checks if value is less than zero"""
         if value < 0:
-            raise ValueError("height must be >= than 0")
-        """assign value to private attribute height"""
+            raise ValueError("height must be >= 0")
         self.__height = value
