c0538b923fe2c3978dd2aa86f29b48c0aa500ade
Bunmi Akinola <bunconice@gmail.com>
2023-10-11 20:22:06 +0100

    updated

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,22 +1,18 @@
 #!/usr/bin/python3
-""""adds all arguments to a Python list"""
+"""
+Module 7-save_to_json_file
 
-import sys
-from unicodedata import name
+Contains function that writes Python obj to file using JSON represenation
+"""
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__(
-        '6-load_from_json_file').load_from_json_file
 
-    name = "add_item.json"
-    arg = sys.argv[1:]
+def save_to_json_file(my_obj, filename):
+    """Writes Python obj to file using JSON represenation
+    Args:
+        my_obj: python object
+        filename: file
+    """
+    import json
 
-    try:
-        a = load_from_json_file(name)
-
-    except FileNotFoundError:
-        a = []
-
-    a.extend(arg)
-    save_to_json_file(a, name)
+    with open(filename, mode="w", encoding="utf-8") as f:
+        json.dump(my_obj, f)
