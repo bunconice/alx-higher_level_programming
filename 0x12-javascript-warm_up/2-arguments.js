5e858d4df8b546554f2ac2d2c321dca8861c6d31
Bunmi Akinola <bunconice@gmail.com>
2023-12-11 19:34:47 +0100

    script that prints a message depending of the number of arguments passed

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const numArguments = process.argv.length - 2;
+
+// Print the message based on the number of arguments
+if (numArguments === 0) {
+  console.log('No argument');
+} else if (numArguments === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
