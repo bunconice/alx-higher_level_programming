b00b359820a8c59882006c5a033a92bc00985e52
Bunmi Akinola <bunconice@gmail.com>
2023-12-13 17:07:14 +0100

    updated

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -5,7 +5,7 @@ class Square extends require('./5-square.js') {
       this.print();
     } else {
       for (let index = 0; index < this.height; index++) {
-        console.log('C'.repeat(this.width));
+        console.log(c.repeat(this.width));
       }
     }
   }
