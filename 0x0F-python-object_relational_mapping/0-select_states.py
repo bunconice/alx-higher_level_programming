28cf67f09bf7e579811909be080ce143f11f589d
Bunmi Akinola <bunconice@gmail.com>
2023-12-17 23:12:42 +0100

    updated

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,25 +6,25 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-  #connect to the database using arguments from the cli
-  my_db = MySQLdb.connect(
-    host="localhost", user=argv[1], passwd=argv[2], db=argv[3], port=3306)
-  
-  #create cursor object
-  cur = my_db.cursor()
-  
-  #executing mysql queries
-  cur.execute("SELECT * FROM state ORDER BY state.id ASC")
-  
-  #obtaining query result
-  my_data = cur.fetall()
-  
-  #iterate through the data and print
-  for row in my_data:
-    print(row)
-    
-# Close all cursors
-cur.close()
+    # connect to the database using arguments from the cli
+    my_db = MySQLdb.connect(
+        host="localhost", user=argv[1], passwd=argv[2], db=argv[3], port=3306)
 
-# Close all databases
-my_db.close()
+    # create cursor object
+    cur = my_db.cursor()
+
+    # executing mysql queries
+    cur.execute("SELECT * FROM state ORDER BY state.id ASC")
+
+    # obtaining query result
+    my_data = cur.fetall()
+
+    # iterate through the data and print
+    for row in my_data:
+        print(row)
+
+    # Close all cursors
+    cur.close()
+
+    # Close all databases
+    my_db.close()
