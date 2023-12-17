b3df7d9b769ee42dd6272a975d6f41bd078a3b53
Bunmi Akinola <bunconice@gmail.com>
2023-12-18 00:17:25 +0100

    updated

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,28 +1,22 @@
 #!/usr/bin/python3
-
-"""
-Module that connects python script to a database
-"""
+"""lists all State objects, and corresponding
+City objects, contained in the database hbtn_0e_101_usa"""
 from sys import argv
-from sqlalchemy import create_engine
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
-from relationship_city import Base, City
-from relationship_state import State
 
 if __name__ == "__main__":
-
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost/{}'
-        .format(argv[1], argv[2], argv[3]),
-        pool_pre_ping=True
-    )
+        .format(argv[1], argv[2],
+                argv[3]), pool_pre_ping=True)
 
-    my_session_maker = sessionmaker(bind=engine)
-    my_session = my_session_maker()
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    for state in my_session.query(State).order_by(State.id):
+    for state in session.query(State).order_by(State.id):
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
-            print("    {}: {}".format(city.id, city.name))
-
-    my_session.close()
+            print("\t{}: {}".format(city.id, city.name))
