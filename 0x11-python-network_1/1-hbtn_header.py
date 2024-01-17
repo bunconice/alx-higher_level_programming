a80062b1e46ad85da0a869e0390989e3f41f5dc5
Bunmi Akinola <bunconice@gmail.com>
2024-01-17 13:01:56 +0100

    script that prints value of an header

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+script that takes in a URL,
+sends a request to the URL and displays the value of the
+X-Request-Id variable found in the header of the response.
+"""
+from sys import argv
+from urllib.request import urlopen
+
+if __name__ == "__main__":
+    with urlopen(argv[1]) as response:
+        body = response.headers["X-Request-Id"]
+        print(body)
