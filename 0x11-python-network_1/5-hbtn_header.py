b3027f7211f7427708482df8d78943407ff4dc7b
Bunmi Akinola <bunconice@gmail.com>
2024-01-17 18:47:11 +0100

    script that get the value of an header

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+script that takes in a URL,
+sends a request to the URL and
+displays the value of the variable X-Request-Id in the response header
+"""
+from requests import get
+from sys import argv
+
+r = get(argv[1])
+print(r.headers["X-Request-Id"])
