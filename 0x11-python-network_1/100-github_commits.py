07bc6e875acc952cc1ad63598488a77c76c4798d
Bunmi Akinola <bunconice@gmail.com>
2024-01-18 10:00:49 +0100

    script that list 10 commits

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -10,7 +10,8 @@ from requests import get
 if __name__ == "__main__":
     repo = argv[1]
     user = argv[2]
-    url = 'https://developer.github.com/v3/repos/{}/{}/commits/'.format(user, repo)
+    url = 'https://developer.github.com/v3/repos/{}/{}/commits/'.format(
+        user, repo)
     r = get(url)
     commit_list = r.json()
     for commit in commit_list[0:10]:
